d77c0e409c5d6a43fda9ca41d9a8f545ceeee8cc
Angelopoulos <87050409+christianangelopoulos@users.noreply.github.com>
2025-05-16 14:19:36 -0500

    skeleton for migration

---
+++ src/5_New_Token_Migration/TokenMigrationProposal.sol
@@ -0,0 +1,10 @@
+import {Proposal} from "currency-1.5/governance/community/proposals/Proposal.sol";
+
+contract TokenMigrationProposal is Proposal {
+
+// need the address of the migration contract
+// need to give burn permission on ECOx to the migration contract
+// need to give mint permission on the new token to the migration contract
+
+
+}
